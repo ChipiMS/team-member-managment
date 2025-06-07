b01b74fc45e75a0e7bffdc3e76d782fe726a4af5
ChipiSyncfy <chipi@paybook.me>
2025-06-06 19:35:58 -0600

    [update] Add store

--- spa/src/app/app.config.ts
+++ spa/src/app/app.config.ts
@@ -3,9 +3,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
 import  Aura  from '@primeng/themes/aura';
+import { provideStore } from '@ngxs/store';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),providePrimeNG({theme:{
     preset: Aura
-  }})]
+  }}), provideStore(
+[],
+)]
 };
